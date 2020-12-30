603137556e01f6b852a5205539a11e28666c012c
Aditya Kristyanto <42263468+aadiityaak@users.noreply.github.com>
2020-12-30 15:26:38 +0700

    Add sample customizer

--- functions.php
+++ functions.php
@@ -26,7 +26,66 @@ if( ! function_exists( 'justg_child_enqueue_parent_style') ) {
 		$version = $theme->get( 'Version' );
 		// Load the stylesheet
 		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'velocity-style' ), $version );
-		
 	}
+}
+
+add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
+
+function your_parent_theme_setup() {
+	
+	// Load justg_child_enqueue_parent_style after theme setup
 	add_action( 'wp_enqueue_scripts', 'justg_child_enqueue_parent_style' );
+
+	
+	/**
+	* Customizer control in child themes
+	* Sample Panel
+	* 
+	*/ 
+	Kirki::add_panel('panel_home', [
+		'priority'    => 10,
+		'title'       => esc_html__('Home', 'justg'),
+		'description' => esc_html__('', 'justg'),
+	]);
+
+	/**
+	* Sample section
+	* 
+	*/ 
+	Kirki::add_section('home_slider', [
+		'panel'    => 'panel_home',
+		'title'    => __('Slider', 'justg'),
+		'priority' => 10,
+	]);
+
+	/**
+	* Sample Field
+	* 
+	*/ 
+	Kirki::add_field( 'justg_config', [
+		'type'        => 'repeater',
+		'label'       => esc_html__( 'Slider Home', 'justg' ),
+		'section'     => 'home_slider',
+		'priority'    => 10,
+		'row_label' => [
+			'type'  => 'text',
+			'value' => esc_html__( 'Slide', 'justg' ),
+		],
+		'button_label' => esc_html__('Tambah Slide', 'justg' ),
+		'settings'     => 'home_slider_setting',
+		'fields' => [
+			'image' => [
+				'type'        => 'image',
+				'label'       => esc_html__( 'Gambar', 'justg' ),
+				'description' => esc_html__( 'gunakan gambar dengan ukuran sama', 'justg' ),
+				'default'     => '',
+			],
+			'link_url'  => [
+				'type'        => 'text',
+				'label'       => esc_html__( 'Url slide', 'justg' ),
+				'description' => esc_html__( 'link saat gambar di klik', 'justg' ),
+				'default'     => '',
+			],
+		]
+	] );
 }
